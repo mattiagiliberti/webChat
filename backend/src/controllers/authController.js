6e606b3bd6488bf2355f29ca966de05ca3b5f48a
mattiagiliberti <marechiaromattia17@gmail.com>
2025-03-03 19:01:17 +0100

    backend test login and register

---
+++ backend/src/controllers/authController.js
@@ -0,0 +1,48 @@
+const bcrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken");
+const User = require("../models/Users.js");
+
+const register = async (req, res) => {
+    try {
+        const { username, password } = req.body;
+        
+        // Controlla se l'utente esiste già
+        const existingUser = await User.findOne({ username });
+        if (existingUser) return res.status(400).json({ message: "Username già in uso" });
+
+        // Hash della password
+        const hashedPassword = await bcrypt.hash(password, 10);
+
+        // Creazione utente
+        const newUser = new User({ username, password: hashedPassword });
+        await newUser.save();
+
+        res.status(201).json({ message: "Registrazione completata" });
+    } catch (error) {        
+        res.status(500).json({ message: "Errore nel server" });
+    }
+};
+
+const login = async (req, res) => {
+    try {
+        //console.log(req);
+        const { username, password } = req.body;
+        // Trova l'utente
+        const user = await User.findOne({ username });
+        
+        if (!user) return res.status(400).json({ message: "Credenziali non valide" });
+
+        // Verifica la password
+        const isMatch = await bcrypt.compare(password, user.password);
+        if (!isMatch) return res.status(400).json({ message: "Credenziali non valide" });
+
+        // Genera token JWT
+        const token = jwt.sign({ userId: user._id }, process.env.JWT_SECRET, { expiresIn: "7d" });
+
+        res.json({ token, userId: user._id, username: user.username });
+    } catch (error) {
+        res.status(500).json({ message: "Errore nel server" });
+    }
+};
+
+module.exports = { register, login };
--- backend/src/server.js
+++ backend/src/server.js
@@ -3,6 +3,7 @@ const http = require("http");
 const cors = require("cors");
 const dotenv = require("dotenv");
 const connectDB = require("../utils/db");
+const authRoutes = require("./routes/auth");
 
 dotenv.config();
 connectDB();
@@ -12,6 +13,10 @@ const server = http.createServer(app);
 
 app.use(cors());
 app.use(express.json());
+
+
+app.use("/api/auth", authRoutes);
+
 
 server.listen(process.env.PORT, () => {
     console.log(`Server avviato sulla porta ${process.env.PORT}`);
