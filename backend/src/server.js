e7476e6fee4f0a1f347805fd3b210d08b75eec8d
mattiagiliberti <marechiaromattia17@gmail.com>
2025-03-03 15:49:37 +0100

    config backend with db connection and setup frontend Vue

---
+++ backend/src/server.js
@@ -0,0 +1,18 @@
+const express = require("express");
+const http = require("http");
+const cors = require("cors");
+const dotenv = require("dotenv");
+const connectDB = require("../utils/db");
+
+dotenv.config();
+connectDB();
+
+const app = express();
+const server = http.createServer(app);
+
+app.use(cors());
+app.use(express.json());
+
+server.listen(process.env.PORT, () => {
+    console.log(`Server avviato sulla porta ${process.env.PORT}`);
+});
